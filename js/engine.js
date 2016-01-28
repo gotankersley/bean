32d7648541dd33986efa7963255ac4e475c16485
gotankersley <gotankersley@harding.edu>
2016-01-27 20:42:43 -0600

    More

--- js/engine.js
+++ js/engine.js
@@ -1,18 +1,14 @@
+'use strict'
+
 //Constants
 var ARROWS_PATH = './arrows';
-var ARROWS_TO_LOAD = ['up.png', 'forward.png', 'back.png']; //Load just these images, and rotate / translate them to get other directions
+//var ARROWS_TO_LOAD = ['up.png', 'forward.png', 'back.png']; //Load just these images, and rotate / translate them to get other directions
+var ARROWS_TO_LOAD = ['up.png', 'down.png', 'left.png', 'right.png', 'forward.png', 'back.png']; //Load just these images, and rotate / translate them to get other directions
 var SCENES_PATH = './scenes';
-var SCENES_COUNT = 40;
+var SCENES_COUNT = 48;
 
 var CANVAS_SIZE_X = 1024;
 var CANVAS_SIZE_Y = 640;
-
-//Enums
-//var DIR_UP = 'up.png';
-//var DIR_DOWN = 'down.png';
-//var DIR_LEFT = 'left.png';
-//var DIR_RIGHT = 'right.png';
-//var DIR_FORWARD = 'forward.png';
 
 
 //Class Engine
@@ -24,7 +20,9 @@ function Engine(canvasId) {
 	this.cursorX = 0;
 	this.cursorY = 0;
 	this.scenes = [null];	
-	this.curScene = 22; //Invalid
+	
+	this.curScene = 22; //start
+	this.curArrow = 1;
 	
 	//Start
 	var self = this;
@@ -79,30 +77,60 @@ Engine.prototype.run = function() {
 
 Engine.prototype.onMouseClick = function(e) {
 	//Get direction based on position
-	x = e.clientX - this.canvasBounds.left; 
-	y = e.clientY - this.canvasBounds.top;  
+	var x = e.clientX - this.canvasBounds.left; 
+	var y = e.clientY - this.canvasBounds.top;  
 		
 	this.curScene = nav[this.curScene].f;
+	
 }
 
 Engine.prototype.onMouseMove = function(e) {	
-	this.cursorX = e.clientX - this.canvasBounds.left; 
-	this.cursorY = e.clientY - this.canvasBounds.top;  
-	
+	var x = e.clientX - this.canvasBounds.left; 
+	var y = e.cursorY = e.clientY - this.canvasBounds.top;  
+	var point = [x, y];
+	var polygon = [[341,639], [413,396], [415,0], [934,1], [879,572], [919,637], [341,639]];
+	if (inside(point, polygon)) this.curArrow = 2;
+	else this.curArrow = 1;
+	this.cursorX = x;
+	this.cursorY = y;
 }
 
 
 Engine.prototype.onDraw = function() {
 	var ctx = this.ctx;
-	scene = this.scenes[this.curScene];
+	var scene = this.scenes[this.curScene];
 	ctx.drawImage(scene, 0, 0, CANVAS_SIZE_X, CANVAS_SIZE_Y);
 	
 	//Draw navigation arrow
-	var arrow = this.arrows[1];
+	var arrow = this.arrows[this.curArrow];
 	ctx.drawImage(arrow, this.cursorX - 150, this.cursorY - 100, 100, 50);
 	
 	window.requestAnimationFrame(this.onDraw.bind(this));
 }
+
+//Collision detection
+function inside (point, vs) { //https://github.com/substack/point-in-polygon
+    // ray-casting algorithm based on
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    
+    var x = point[0], y = point[1];
+    
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+        
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+};
+
+
+
+//Engine.prototype.drawArrow(ctx, dir) {}
 //End class Engine
 
 var engine = new Engine('canvas'); //Init
